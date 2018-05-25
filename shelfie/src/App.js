dd93a1d099f7302c3ba6ec600e8f17a949d0b4d2
Erik Amundson <SavenTheChickens@gmail.com>
2018-05-25 11:05:26 -0600

    first commit

---
+++ shelfie/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Header from './component/Header/Header';
+import Product from './component/Product/Product';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Dashboard />
+        <Form />
+        <Header />
+        <Product />
+      </div>
+    );
+  }
+}
+
+export default App;
